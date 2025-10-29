a4b4f171757a548afb449142cffc05a383e970f0
theBappy <abappy575@gmail.com>
2025-10-29 13:20:49 +0600

    implement subscription hooks and polar client integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +12,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignup: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "0f8d6ef2-31f8-408e-9faa-03a1b2ee0ff3",
+              slug: "FlowNexus-Pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
