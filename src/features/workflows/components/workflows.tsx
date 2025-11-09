ac9ded82fdcb07219ffc38c12e3431030c2e82a1
theBappy <abappy575@gmail.com>
2025-11-09 10:13:37 +0600

    Add workflows pagination and search functionality with nuqs integration
    - Introduced EntitySearch and EntityPagination components for improved workflows management.
    - Updated workflows router to support pagination and search parameters.
    - Integrated nuqs for state management of workflows parameters.
    - Enhanced User model in Prisma schema for unique email validation.
    - Added constants for pagination , search functionality for workflows.

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import {
+  EntityContainer,
+  EntityHeader,
+  EntityPagination,
+  EntitySearch,
+} from "@/components/entity-components";
 import {
   useSuspenseWorkflows,
   useCreateWorkflow,
@@ -8,6 +13,24 @@ import {
 import { ReactNode } from "react";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
+import { useWorkflowsParams } from "../hooks/use-workflows-params";
+import { useEntitySearch } from "../hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+  const [params, setParams] = useWorkflowsParams();
+  const { searchValue, onSearchChange } = useEntitySearch({
+    params,
+    setParams,
+  });
+
+  return (
+    <EntitySearch
+      value={searchValue}
+      onChange={onSearchChange}
+      placeholder="Search workflows"
+    />
+  );
+};
 
 export const WorkflowsList = () => {
   const workflows = useSuspenseWorkflows();
@@ -22,7 +45,7 @@ export const WorkflowsList = () => {
 export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
   const createWorkflow = useCreateWorkflow();
   const { handleError, modal } = useUpgradeModal();
-  const router = useRouter()
+  const router = useRouter();
 
   const handleCreate = () => {
     createWorkflow.mutate(undefined, {
@@ -50,12 +73,26 @@ export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
   );
 };
 
+export const WorkflowsPagination = () => {
+  const workflows = useSuspenseWorkflows()
+  const [params, setParams] = useWorkflowsParams()
+
+  return (
+    <EntityPagination 
+    disabled={workflows.isFetching}
+    totalPages={workflows.data.totalPages}
+    page={workflows.data.page}
+    onPageChange={(page) => setParams({...params, page})}
+    />
+  )
+}
+
 export const WorkflowsContainer = ({ children }: { children: ReactNode }) => {
   return (
     <EntityContainer
       header={<WorkflowsHeader />}
-      search={<></>}
-      pagination={<></>}
+      search={<WorkflowsSearch />}
+      pagination={<WorkflowsPagination />}
     >
       {children}
     </EntityContainer>
